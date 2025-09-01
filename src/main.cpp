6cb55e0e1a3cce8d19fe02ee6460662228350523
DarthMooMancer <dmm.dev@pm.me>
2025-09-01 00:46:11 -0400

    feat: window and input handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <thread>
+#include "input.hpp"
+#include "window.hpp"
 
 int main() {
-	std::cout << "Hello World" << std::endl;
+	Input input;
+	Window window;
+
+	bool running = true;
+	std::thread input_thread(&Input::get_input, &input, std::ref(running));
+
+	while (running) {
+		std::cout << "Hello World" << std::endl;
+		window.update_display();
+	
+	}
+	input_thread.join();
 	return 0;
 }
